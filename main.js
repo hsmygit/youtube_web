f5ce4f4439b2d8fe38dc10d946b9165a79f7b342
hsmygit <132031871+hsmygit@users.noreply.github.com>
2023-07-30 22:08:49 +0900

    Add files via upload
    action버튼 클릭시 파란색으로 변함 , 구독 버튼 누르면 구동중으로 변함 기능 추가.

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
 });
+
+
+const actionButtons = document.querySelectorAll('.info .actions button');
+
+
+actionButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.querySelector('i').classList.toggle('active');
+  });
+});
+
+const subscribeBtn = document.getElementById('subscribeBtn');
+
+subscribeBtn.addEventListener('click', () => {
+  if (subscribeBtn.textContent === '구독하기') {
+    subscribeBtn.textContent = '구독중';
+  } else {
+    subscribeBtn.textContent = '구독하기';
+  }
+});
+
